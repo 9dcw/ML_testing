827aa7937a94d9c08417fa06dd05b68eec81a1c2
9dcw <Maximus1!>
2016-02-01 08:16:42 -0500

    Adding some training scripts

--- general_testing.py
+++ general_testing.py
@@ -17,70 +17,113 @@ import sys
 from sklearn.grid_search import GridSearchCV
 
 
-print 'loading data'
-boston = datasets.load_boston()
-#iris = datasets.load_iris()
-print 'data loaded'
-X, y = boston.data, boston.target
-#X, y = iris.data, iris.target
-print 'X shape:', X.shape
-print 'y shape:', y.shape
 
-scaler = StandardScaler()
-
-# I need to fit and transform the data with the scaler.. how do I put
-# this into pipeline?
+def main():
+    print 'loading data'
+    boston = datasets.load_boston()
+    #iris = datasets.load_iris()
+    print 'data loaded'
+    X, y = boston.data, boston.target
+    #X, y = iris.data, iris.target
+    print 'X shape:', X.shape
+    print 'y shape:', y.shape
 
 
-# initialize PCA to pick 5 components
-pca = decomposition.PCA(n_components=4)
+    scaler = StandardScaler()
 
-pcaX = scaler.fit_transform(X)
-pca.fit(pcaX)
-
-plt.figure(1, figsize=(4, 3))
-plt.clf()
-plt.plot(pca.explained_variance_ratio_, linewidth=2)
-plt.axis('tight')
-plt.xlabel('n_components')
-plt.ylabel('explained variance')
-plt.show()
-sys.exit()
-# initialize selectKBest to pick the best of the ones that occur naturally
-# the feature union does not check whether there is overlap between the estimators
-# so we need to seriously watch out for this...
-selection = SelectKBest(k=1)
-
-# build a dict with these for pipeline purposes
-combined_features = FeatureUnion([('pca', pca), ('univ_select', selection)])
-
-# use the combined features to transform the dataset
-X_features = combined_features.fit(X, y).transform(X)
-
-# initialize the svm
-svm = SVR(kernel="linear")
-
-# I think I put the scaler into the first set of the pipeline...
-
-pipeline = Pipeline([('scaler', scaler), ('features', combined_features), ('svm', svm)])
-
-param_grid = dict(features__pca__n_components=[2, 5, 10],
-                  features__univ_select__k=[1, 2],
-                  svm__C=[0.1, 1, 10])
-
-#scoring: precision, accuracy, recall,
-
-grid_search = GridSearchCV(pipeline, param_grid=param_grid, verbose=10)
-grid_search.fit(X, y)
-print grid_search.best_estimator_
-
-scores = grid_search.grid_scores_
+    # I need to fit and transform the data with the scaler.. how do I put
+    # this into pipeline?
 
 
-fig, ax = plt.subplots()
+    # initialize PCA to pick 5 components
+    pca = decomposition.PCA(n_components=4)
 
-ax.scatter(y, predicted)
-ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-ax.set_xlabel('Measured')
-ax.set_ylabel('Predicted')
-plt.show()
+    scaledX = scaler.fit_transform(X)
+
+
+    # I want to build my cross validation picks
+    # then I will plot partial dependence to see how the features work
+    # then I will PCA and plot partial dependence there
+    # then lasso PCA
+
+    # then select the parameters
+    # plots are nice but shouldn't I be selecting based on multi-dimensional data?
+
+    # then generate a list of multi-parameter algorithms
+    # then do a parameter search with gradient boost and a few other multi-parameter algorithms
+    #
+
+    X_train, y_train, X_test, y_test =
+    
+    clf = GradientBoostingRegressor(n_estimators=100, max_depth=4,
+                                    learning_rate=.1, loss='huber',
+                                    random_state=1)
+
+    print 'training'
+    clf.fit(X_train, y_train)
+    print 'trained'
+
+    features = [0, 5, 1, 2, (5, 1)]
+    fig, axs = plot_partial_dependence(clf, X_train, features, feature_names=names,
+                                       n_jobs=3, grid_resolution=50)
+
+    fig.suptitle('Partial dependence of house value on nonlocation features\n' + 'for the California housing dataset')
+    plt.subplots_adjust(top=0.9)
+
+    plt.show()
+
+
+
+    pca.fit(pcaX)
+
+    plt.figure(1, figsize=(4, 3))
+    plt.clf()
+    plt.plot(pca.explained_variance_ratio_, linewidth=2)
+    plt.axis('tight')
+    plt.xlabel('n_components')
+    plt.ylabel('explained variance')
+    plt.show()
+
+    # initialize selectKBest to pick the best of the ones that occur naturally
+    # the feature union does not check whether there is overlap between the estimators
+    # so we need to seriously watch out for this...
+    selection = SelectKBest(k=1)
+
+    # build a dict with these for pipeline purposes
+    combined_features = FeatureUnion([('pca', pca), ('univ_select', selection)])
+
+    # use the combined features to transform the dataset
+    X_features = combined_features.fit(X, y).transform(X)
+
+    # initialize the svm
+    svm = SVR(kernel="linear")
+
+    # I think I put the scaler into the first set of the pipeline...
+
+    pipeline = Pipeline([('scaler', scaler), ('features', combined_features), ('svm', svm)])
+
+    param_grid = dict(features__pca__n_components=[2, 5, 10],
+                      features__univ_select__k=[1, 2],
+                      svm__C=[0.1, 1, 10])
+
+    #scoring: precision, accuracy, recall,
+
+    grid_search = GridSearchCV(pipeline, param_grid=param_grid, verbose=10)
+    grid_search.fit(X, y)
+    print grid_search.best_estimator_
+
+    scores = grid_search.grid_scores_
+
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(y, predicted)
+    ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+    ax.set_xlabel('Measured')
+    ax.set_ylabel('Predicted')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
--- partial_dependence_plots.py
+++ partial_dependence_plots.py
@@ -1,3 +1,4 @@
+print 'loading libraries'
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -6,56 +7,66 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble.partial_dependence import plot_partial_dependence
 from sklearn.ensemble.partial_dependence import partial_dependence
 from sklearn.datasets.california_housing import fetch_california_housing
+import sys
+print 'libraries loaded'
 
 
-cal_housing = fetch_california_housing()
+def main():
 
-X_train, X_test, y_train, y_test = train_test_split(cal_housing.data,
-                                                    cal_housing.target,
-                                                    test_size=0.2,
-                                                    random_state=1)
-names = cal_housing.feature_names
+    cal_housing = fetch_california_housing()
 
-clf = GradientBoostingRegressor(n_estimators=100, max_depth=4,
-                                learning_rate=.1, loss='huber',
-                                random_state=1)
+    print cal_housing.data.shape
+    print cal_housing.target.shape
 
-print 'training'
-clf.fit(X_train, y_test)
-print 'trained'
 
-features = [0, 5, 1, 2, (5, 1)]
-fig, axs = plot_partial_dependence(clf, X_train, features, feature_names=names,
-                                   n_jobs=3, grid_resolution=50)
+    X_train, X_test, y_train, y_test = train_test_split(cal_housing.data,
+                                                        cal_housing.target,
+                                                        test_size=0.2,
+                                                        random_state=1)
+    names = cal_housing.feature_names
 
-fig.suptitle('Partial dependence of house value on nonlocation features\n' + 'for the California housing dataset')
-plt.subplots_adjust(top=0.9)
+    clf = GradientBoostingRegressor(n_estimators=100, max_depth=4,
+                                    learning_rate=.1, loss='huber',
+                                    random_state=1)
 
-plt.show()
+    print 'training'
+    clf.fit(X_train, y_train)
+    print 'trained'
 
-print '_' * 80
-print 'custom 3d plot via "partial_dependence'
+    features = [0, 5, 1, 2, (5, 1)]
+    fig, axs = plot_partial_dependence(clf, X_train, features, feature_names=names,
+                                       n_jobs=3, grid_resolution=50)
 
-fig = plt.figure()
+    fig.suptitle('Partial dependence of house value on nonlocation features\n' + 'for the California housing dataset')
+    plt.subplots_adjust(top=0.9)
 
-target_feature = (1,5)
-pdp, (x_axis, y_axis) = partial_dependence(clf, target_feature, X=X_train,
-                                           grid_resolution=50)
+    plt.show()
 
-XX, YY = np.meshgrid(x_axis, y_axis)
-Z = pdp.T.reshape(XX.shape).T
+    print '_' * 80
+    print 'custom 3d plot via "partial_dependence'
 
-ax = Axes3D(fig)
-surf = ax.plot_surface(XX, YY, Z, rstride=1, cstride=1, cmap=plt.cm.BuPu)
+    fig = plt.figure()
 
-ax.set_xlabel(names[target_feature[0]])
-ax.set_ylabel(names[target_feature[1]])
-ax.set_zlabel('partial dependence')
-# pretty init view
-ax.view_init(elev=22, azim=122)
-plt.colorbar(surf)
-plt.suptitle('partial dependence of house value on median age and avg occupancy')
+    target_feature = (1,5)
+    pdp, (x_axis, y_axis) = partial_dependence(clf, target_feature, X=X_train,
+                                               grid_resolution=50)
 
-plt.subplots_adjust(top=0.9)
+    XX, YY = np.meshgrid(x_axis, y_axis)
+    Z = pdp.T.reshape(XX.shape).T
 
-plt.show()
+    ax = Axes3D(fig)
+    surf = ax.plot_surface(XX, YY, Z, rstride=1, cstride=1, cmap=plt.cm.BuPu)
+
+    ax.set_xlabel(names[target_feature[0]])
+    ax.set_ylabel(names[target_feature[1]])
+    ax.set_zlabel('partial dependence')
+    # pretty init view
+    ax.view_init(elev=22, azim=122)
+    plt.colorbar(surf)
+    plt.suptitle('partial dependence of house value on median age and avg occupancy')
+
+    plt.subplots_adjust(top=0.9)
+
+    plt.show()
+if __name__ == '__main__':
+    main()
